5e62e3d7a1e7693c8d405cd69c0a54b4188e821b
Waheed-Labib <labibwahid28@gmail.com>
2023-04-06 00:13:33 +0600

    some more header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import './Header.css'
+import './Header.css';
 
 
 const Header = () => {
+
     return (
+
         <div className='header d-flex justify-content-between align-items-center'>
 
             <a className='site-name' href={'/'}>Quizziz</a>
             <nav>
-                <NavLink to={'/home'} className={`nav-option me-3 ${(isActive) => isActive ? 'active' : ''}`}>Home</NavLink>
+                <NavLink to={'/home'} className={`nav-option me-3 ${(isActive) => isActive ? 'active' : ''} d-none d-sm-inline`}>Home</NavLink>
                 <NavLink to={'/topics'} className={'nav-option me-3'}>Topics</NavLink>
                 <NavLink to={'/statistics'} className={'nav-option me-3'}>Statistics</NavLink>
                 <NavLink to={'/blogs'} className={'nav-option me-3'}>Blogs</NavLink>
             </nav>
 
         </div>
+
+
+
+
     );
 };
 
